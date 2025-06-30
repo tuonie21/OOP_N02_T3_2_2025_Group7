ab2b5fab29f64aefd410a5910ec7d680ba8bc542
Trần Quang Trường <24100073@st.phenikaa-uni.edu.vn>
2025-06-30 10:02:03 +0000

    Cap Nhat Thu Vien RemoteLibrary

--- QuanLyBanHang/src/run.sh
+++ QuanLyBanHang/src/run.sh
@@ -1,1 +1,5 @@
+filename=$(basename "$1" .java)
+javac "$1" || exit 1
+java "$filename"
+rm "${filename}.class"
 
--- QuanLyBanHang/test/run.sh
+++ QuanLyBanHang/test/run.sh
@@ -1,1 +1,4 @@
-
+filename=$(basename "$1" .java)
+javac "$1" || exit 1
+java "$filename"
+rm "${filename}.class"
