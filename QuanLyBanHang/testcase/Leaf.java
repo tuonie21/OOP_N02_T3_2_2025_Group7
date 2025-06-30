60f0732145c02eba9c6cd5a255ed82a67a6dcec7
Trần Quang Trường <24100073@st.phenikaa-uni.edu.vn>
2025-06-30 10:08:43 +0000

    Cap Nhat Thu Vien RemoteLibrary

---
+++ QuanLyBanHang/testcase/Leaf.java
@@ -0,0 +1,12 @@
+public class Leaf {
+    int i = 0;
+    Leaf increment() {
+        i++;
+        return this;//Leaf
+    }
+    void print() {System.out.println("i = " + i);}
+    public static void main(String[] args) {
+        Leaf x = new Leaf();
+        x.increment().increment().increment().print(); //3
+    }
+}
